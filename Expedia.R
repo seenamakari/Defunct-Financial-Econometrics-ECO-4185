fa01800fc7b3e5f08a745e5a1f889e1472c05f1e
seenamakari <39891936+seenamakari@users.noreply.github.com>
2023-09-17 22:36:27 -0400

    Add files via upload

---
+++ Expedia.R
@@ -0,0 +1,13 @@
+# Read the CSV file into an R data frame
+data <- read.csv("C:/Users/16475/Desktop/EXPE.csv")
+
+# Generate a summary of the data
+summary_data <- summary(data)
+
+# Print the summary to the R console
+print(summary_data)
+
+
+
+
+
