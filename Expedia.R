35e7b04b856982fbc71d987b9dc68b2b57f4b3ca
seenamakari <39891936+seenamakari@users.noreply.github.com>
2023-09-18 20:15:40 -0400

    Updated R file

--- Expedia.R
+++ Expedia.R
@@ -1,11 +1,22 @@
 # Read the CSV file into an R data frame
 data <- read.csv("C:/Users/16475/Desktop/EXPE.csv")
 
-# Generate a summary of the data
-summary_data <- summary(data)
+# Check if data was successfully loaded
+if (exists("data")) {
+  # Generate a summary of the data
+  summary_data <- summary(data)
+  
+  # Print the summary to the R console
+  print(summary_data)
+} else {
+  # Print an error message if data loading fails
+  print("Error: Unable to load the CSV file.\n")
+}
 
-# Print the summary to the R console
-print(summary_data)
+price_relative <- (data$Open)/(data$Close) #Have to figure out how to subtract 1
+
+print(price_relative)
+
 
 
 
